ce4b081c033c27f64ef77d87aa8d1f16bbfbb4c4
Miquelino <reynaldomiquelinoneto@hotmail.com>
2025-09-26 10:21:13 -0300

    Continuando o curso Spring Boot 3: Aplique boas práticas e proteja uma API Rest

---
+++ Spring Boot 3 Aplique boas praticas e protea uma API Rest/2770-spring-boot-projeto_inicial/src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratarError400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors(); // <-- retorna List<FieldError>
+        var dto   = erros.stream()
+                .map(DadosErrosValidacao::new)
+                .toList();
+        return ResponseEntity.badRequest().body(dto);
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+}
