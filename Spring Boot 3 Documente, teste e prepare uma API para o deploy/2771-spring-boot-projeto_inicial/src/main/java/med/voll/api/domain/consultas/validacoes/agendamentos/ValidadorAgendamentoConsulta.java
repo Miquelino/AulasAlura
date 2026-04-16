aeaab7a39d362f69a483a4f75ee970820e0a1bb7
Miquelino <reynaldomiquelinoneto@hotmail.com>
2026-04-16 10:19:35 -0300

    Conclusão do curso Microsserviços na prática: mensageria com RabbitMQ

--- Spring Boot 3 Documente, teste e prepare uma API para o deploy/2771-spring-boot-projeto_inicial/src/main/java/med/voll/api/domain/consultas/validacoes/agendamentos/ValidadorAgendamentoConsulta.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.domain.consultas.validacoes;
-
-import med.voll.api.domain.consultas.DadosAgendamentoConsulta;
-
-public interface ValidadorAgendamentoConsulta {
-
-    void validar(DadosAgendamentoConsulta dados);
-
-}
--- Spring Boot 3 Documente, teste e prepare uma API para o deploy/2771-spring-boot-projeto_inicial/src/main/java/med/voll/api/domain/consultas/validacoes/cancelamento/ValidadorCancelamentoDeConsulta.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.domain.consultas.cancelamento;
-
-import med.voll.api.domain.consultas.DadosCancelamentoConsulta;
-
-public interface ValidadorCancelamentoDeConsulta {
-    void validar(DadosCancelamentoConsulta dados);
-}
